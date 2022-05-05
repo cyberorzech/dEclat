ebbd891e90a60faef31c4a142179e84faab9cd1d
cyberorzech <natan.orze@gmail.com>
2022-05-05 18:35:48 -0400

    Add sending requests to recent endpoint

--- run.py
+++ run.py
@@ -29,9 +29,54 @@ from src.logger import initialize_logger
 def auth():
     return os.environ.get("bearer-token")
 
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+def create_url(keyword, start_date, end_date, max_results = 10):
+    """
+    search_url to url do endpointa
+    query_params sa zalezne od endpointu
+    """
+    search_url = "https://api.twitter.com/2/tweets/search/recent"
+    
+    #search_url = "https://api.twitter.com/2/tweets/search/recent" #Change to the endpoint you want to collect data from
+
+    #change params based on the endpoint you are using
+
+    # query_params = {'query': keyword,
+    #                 'start_time': start_date,
+    #                 'end_time': end_date,
+    #                 'max_results': max_results,
+    #                 'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+    #                 'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+    #                 'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+    #                 'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+    #                 'next_token': {}}
+    query_params = {
+        'query': keyword,
+        'next_token': {}
+    }
+    return search_url, query_params
+
+def connect_to_endpoint(url, headers, params, next_token = None):
+    params['next_token'] = next_token   #params object received from create_url function
+    response = requests.request("GET", url, headers = headers, params = params)
+    print("Endpoint Response Code: " + str(response.status_code))
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
 
 def main():
-    logger.success("Good")
+    headers = create_headers(auth())
+    keyword = "cybersecurity lang:en"
+    start_time = "2021-03-01T00:00:00.000Z"
+    end_time = "2021-03-31T00:00:00.000Z"
+    max_results = 15
+
+    url, query_params = create_url(keyword, start_time, end_time, max_results)
+    json_response = connect_to_endpoint(url, headers, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
