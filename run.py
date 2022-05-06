fbb5e00152563dedf4d99ddfbbe6c78ee71fce76
cyberorzech <natan.orze@gmail.com>
2022-05-05 20:53:14 -0400

    Update main script. TODO: nouns

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from loguru import logger
+from tqdm import tqdm
 
 from src.logger import initialize_logger
 from src.tweets_getter import Tweets
@@ -12,9 +13,14 @@ def main():
     tweets = Tweets(bearer_token, "z")
     tweets_content = tweets.get_tweets(1)
     tweets.save_tweets()
+    logger.success("Tweets saved")
     
     contents = convert_tweets_file_content_to_list(tweets_content)
-    print(contents)
+    for tweet in tqdm(contents):
+        tweet = text_to_list(tweet)
+        tweet = alpha_lowercase_list(tweet)
+        tweet = sorted_without_duplicates_list(tweet)
+        # rzeczowniki
 
 
 
