fee22a718b1e8ba8602e6520628b645561f30d81
MatFil99 <filip.mazur123@gmail.com>
2022-05-07 19:15:46 +0200

    First working version of dEclat

--- src/declat.py
+++ src/declat.py
@@ -1,12 +1,13 @@
+from unittest import result
 import pandas as pd
 import numpy as np
 
 class dEclat:
 
     result = dict()
+    minSup = 0
 
-    def dEclat():
-        pass
+    temp0 = list()
 
     def dEclat(minSup: int, data: pd.DataFrame = None, filename: str = None):
         
@@ -18,12 +19,17 @@ class dEclat:
             except:
                 print("Wrong file")
         
+        dEclat.minSup = minSup if minSup else dEclat.minSup
+
         if data is None:
             return
 
-        start_dict, start_items_list  = dEclat.get_items(data)
-        result = start_dict
-        dEclat.dEclat_running(start_items_list, minSup)
+        dEclat.result, start_items_list  = dEclat.get_items(data)
+        # dEclat.result = start_dict
+
+        # dEclat.temp0 = start_items_list
+        dEclat.dEclat_running(start_items_list)
+        # dEclat.convert_to_tids(data.shape[1])
 
     def get_items(data: pd.DataFrame):
         """
@@ -38,30 +44,49 @@ class dEclat:
                 if cell_ij is np.nan:
                     break
 
-                one_length_items_l.append({cell_ij})
-                if cell_ij not in one_length_items_d:
+                if str({cell_ij}) not in one_length_items_d:
                     one_length_items_d[str({cell_ij})] = [1, T-{i}]
+                    one_length_items_l.append({cell_ij})
                 else:
-                    one_length_items_d[cell_ij][0] += 1
-                    one_length_items_d[cell_ij][1] -= {i}
+                    one_length_items_d[str({cell_ij})][0] += 1
+                    one_length_items_d[str({cell_ij})][1] -= {i}
         
-        return one_length_items_d, one_length_items_l
+        one_length_items_clear = list()
 
-    def dEclat_running(P: list, minSup: int):
-        for i in range(0, len(P)-1):
-            print(f"i = {i}")
+        for i in one_length_items_l:
+
+            if one_length_items_d[str(i)][0] <= dEclat.minSup:
+                del one_length_items_d[str(i)]
+            else:
+                one_length_items_clear.append(i)
+                
+        return one_length_items_d, one_length_items_clear
+
+    def dEclat_running(P: list):
+
+        for i in range(0, len(P)):
             Ti = []
             Xi = P[i]
-            for j in range(i+1, len(P)-1):
+            for j in range(i+1, len(P)):
                 Xj = P[j]
                 R = Xi.union(Xj)
                 dR = dEclat.result[str(Xj)][1] - dEclat.result[str(Xi)][1]
-                supR = dEclat.result[str(Xi)][0] - len(dEclat.result[str(Xj)][1])
-                if supR > minSup:
+                supR = dEclat.result[str(Xi)][0] - len(dR)
+                if supR > dEclat.minSup:
                     Ti.append(R)
                     dEclat.result[str(R)] = [supR, dR]
             if not Ti:
-                dEclat.dEclat_running(Ti, minSup)
+                dEclat.dEclat_running(Ti)
+
+    def convert_to_tids(n: int):
+        T = set(range(n))
+
+        for key in dEclat.result.keys():
+            parent_key = dEclat.get_parent_key(key)
+            dEclat.result[key][1] = dEclat.result[parent_key][1] - dEclat.result[key][1] if parent_key else T - dEclat.result[key][1]
+
+    def get_parent_key(key: str):
+        return key[0:key.rfind(',')] + '}' if ',' in key else ""
 
 if __name__ == "__main__":
     dEclat.dEclat(4)
